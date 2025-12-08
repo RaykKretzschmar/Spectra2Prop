8bb7851d80f0e39c0358de30f8733b11cded1577
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2025-12-08 08:54:21 +0100

    Fix SpectraTransformer syntax error

--- spectra2prop/models/__init__.py
+++ spectra2prop/models/__init__.py
@@ -1,1 +1,7 @@
-from .cnn1d import SpectralCNN, SimpleTransformer
+    def forward(self, x):
+                x = self.embedding(x)
+                        x = x.unsqueeze(1)
+                                x = self.transformer_encoder(x)
+                                        x = x.mean(dim=1)
+                                                x = self.fc(x)
+                                                        return xfrom .cnn1d import SpectralCNN, SimpleTransformer
