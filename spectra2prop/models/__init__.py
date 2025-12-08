92c269cb6421b185bcdfec755f1ccd8abab98ab6
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2025-12-08 09:35:16 +0100

    Fix incorrect content in init.py

--- spectra2prop/models/__init__.py
+++ spectra2prop/models/__init__.py
@@ -1,7 +1,2 @@
-    def forward(self, x):
-                x = self.embedding(x)
-                        x = x.unsqueeze(1)
-                                x = self.transformer_encoder(x)
-                                        x = x.mean(dim=1)
-                                                x = self.fc(x)
-                                                        return xfrom .cnn1d import SpectralCNN, SimpleTransformer
+from .cnn1d import SpectralCNN
+from .transformer import SpectraTransformer
